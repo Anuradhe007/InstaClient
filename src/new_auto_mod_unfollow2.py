f54a54799119545c650d9090256d669f99ca2c3b
anuradha <anuradhap@intervest.lk>
2018-10-26 12:02:49 +0530

    first commit

---
+++ src/new_auto_mod_unfollow2.py
@@ -0,0 +1,7 @@
+from .new_unfollow import new_unfollow
+
+
+def new_auto_mod_unfollow2(self):
+    log_string = "Trying to unfollow: %s" % (self.current_user)
+    self.write_log(log_string)
+    new_unfollow(self, self.current_id, self.current_user)
